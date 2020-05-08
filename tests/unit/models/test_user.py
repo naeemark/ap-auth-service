a069e760b452b35f0d62dec5be58c4cc162bb65e
wajahatahmed <wajahatahmad056@gmail.com>
2020-05-08 14:26:01 +0500

    basic unit test for models

---
+++ tests/unit/models/test_user.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from models.user import UserModel
+
+
+class ModelTest(TestCase):
+    def test_create_user(self):
+        user = UserModel("xyz", "hvhj!@#")
+
+        self.assertEqual(user.username, 'xyz', "incorrect username")
+        self.assertEqual(user.password, 'hvhj!@#', "incorrect password")
+
+    def test_item_json(self):
+        user = UserModel("xyz", "hvhj!@#")
+        expected = {
+            'username': 'xyz',
+            'password': 'hvhj!@#'
+        }
+
+        self.assertEqual(user.json(), expected, f"expected {expected}")
