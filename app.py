f09b0e4eaa45a10736f03be4d92cb4a57834182b
wajahatahmed <wajahatahmad056@gmail.com>
2020-05-11 17:33:17 +0500

    environment variables added

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from os import environ
 
 from resources.user import UserRegister, UserLogin
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI") or 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") or False
+app.config['PROPAGATE_EXCEPTIONS'] = environ.get("PROPAGATE_EXCEPTIONS") or True
+app.secret_key = environ.get("SECRET_KEY") or 'jose'
 api = Api(app)
 
 
@@ -25,7 +26,5 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
-
-
     db.init_app(app)
     app.run(port=5000, debug=True)
