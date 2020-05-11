af612795ecda825738c3143ffc0a1eddcb2f16ad
wajahatahmed <wajahatahmad056@gmail.com>
2020-05-11 16:49:12 +0500

    removed unused material

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,6 @@
 from flask_jwt_extended import (create_access_token,
-                                create_refresh_token,
-                                get_jwt_identity)
+                                create_refresh_token
+                                )
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
