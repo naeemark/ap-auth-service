241949b3750c358185ff90fd32227f621a857a87
wajahatahmed <wajahatahmad056@gmail.com>
2020-05-15 17:06:48 +0500

    .env file set for environment variables

--- src/app.py
+++ src/app.py
@@ -1,36 +1,41 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from os import environ
-from flask_sqlalchemy import SQLAlchemy
-from flasgger import Swagger, APISpec
+import os
+from dotenv import load_dotenv
+import pathlib
+
+project_folder = pathlib.Path(__file__).parent.absolute()
+load_dotenv(os.path.join(project_folder, '.env'))
+
+from flasgger import Swagger
 
 from src.resources.user import (UserRegister,
-                            UserLogin,
-                            TokenRefresh,
-                            ChangePassword)
+                                UserLogin,
+                                TokenRefresh,
+                                ChangePassword)
 from src.db import db
 from src.constant.exception import Exception
 
-POSTGRES_URL = environ.get("POSTGRES_URL") or "127.0.0.1:5432"
-POSTGRES_USER = environ.get("POSTGRES_USER") or "postgres"
-POSTGRES_PW = environ.get("POSTGRES_PW") or "test123"
-POSTGRES_DB = environ.get("POSTGRES_DB") or "postgres"
+POSTGRES_URL = os.getenv("POSTGRES_URL") or "127.0.0.1:5432"
+POSTGRES_USER = os.getenv("POSTGRES_USER") or "postgres"
+POSTGRES_PW = os.getenv("POSTGRES_PW") or "test123"
+POSTGRES_DB = os.getenv("POSTGRES_DB") or "postgres"
 
 DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL,
                                                       db=POSTGRES_DB)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") or False
-app.config['PROPAGATE_EXCEPTIONS'] = environ.get("PROPAGATE_EXCEPTIONS") or True
-app.secret_key = environ.get("SECRET_KEY") or 'jose'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS") or False
+app.config['PROPAGATE_EXCEPTIONS'] = os.getenv("PROPAGATE_EXCEPTIONS") or True
+app.secret_key = os.getenv("SECRET_KEY") or 'jose'
 api = Api(app)
 app.config['SWAGGER'] = {
     'title': 'ALETHEA',
     'uiversion': 3,
-    'description':'',
-    'version':"1.0"
+    'description': '',
+    'version': "1.0"
 }
 
 swagger = Swagger(app)
