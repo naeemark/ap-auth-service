e9b6ba5ac8bc6bc84fb152ccde2a6f88674b5e06
Naeem <naeemark@gmail.com>
2020-05-16 23:41:42 +0800

    updates env vars

--- src/app.py
+++ src/app.py
@@ -60,10 +60,9 @@ api.add_resource(TokenRefresh, "/auth/refresh")
 api.add_resource(ChangePassword, "/user/changePassword")
 
 # temporary logging
-list_routes = ["%s" % rule for rule in app.url_map.iter_rules()][0:-1]
-print("Routes:\n", "\n ".join(str(line) for line in list_routes))
+# list_routes = ["%s" % rule for rule in app.url_map.iter_rules()][0:-1]
+# print("Routes:\n", "\n ".join(str(line) for line in list_routes))
 
 
 if __name__ == "__main__":
-    db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run()
