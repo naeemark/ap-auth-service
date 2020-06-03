47b7db71fea1381acd9bc125db71e9ec5aa0e002
wajahatahmed <wajahatahmad056@gmail.com>
2020-06-03 18:29:51 +0500

    test added

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -2,11 +2,17 @@
     A file to containe all unit tests of
     UserModel
 """
+import datetime
+import os
+
+import fakeredis
 import pytest
+from src import create_app
 from src.models.user import UserModel
+from src.utils.blacklist_manager import BlacklistManager
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def new_user():
     """
         Creates and return new User
@@ -23,3 +29,28 @@ def test_new_user(new_user):
     assert new_user is not None
     assert new_user.email == "abc123@gmail.com"
     assert new_user.password == "FlaskIsAwesome"
+
+
+def test_blacklist_manager():
+    """fake redis test"""
+    redis_instance = fakeredis.FakeStrictRedis()
+    flask_app = create_app("flask_test.cfg")
+    flask_app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(
+        minutes=int(os.environ["JWT_ACCESS_TOKEN_EXPIRES"])
+    )
+    BlacklistManager.initialize_redis(flask_app, redis_instance)
+    blacklist_manager = BlacklistManager()
+    blacklist_manager.insert_blacklist_token_id("3", "1231231Xdfwefwe")
+    black_list = blacklist_manager.get_jti_list()
+
+    assert isinstance(black_list, list)
+
+
+def test_redis():
+    """fake redis test"""
+    redis_instance = fakeredis.FakeStrictRedis()
+    redis_instance.set("1", "abc")
+    redis_instance.set("test", "12")
+
+    assert redis_instance.get("test").decode() == "12"
+    assert redis_instance.get("1").decode() == "abc"
