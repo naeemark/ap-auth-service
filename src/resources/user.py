4afb7f99fb7a2cc1f5c0d4901d0f35adcd84f5fc
wajahatahmed <wajahatahmad056@gmail.com>
2020-06-03 10:38:05 +0500

    '/user/register' error case added

--- src/resources/user.py
+++ src/resources/user.py
@@ -43,7 +43,23 @@ class UserRegister(Resource):
         password = data["password"]
 
         if UserModel.find_by_email(email):
-            return {"message": ValidationException.USER_ALREADY_EXISTS}, 400
+            response = {
+                "errors": [
+                    {
+                        "errorCode": "VALIDATION_ERROR",
+                        "errorTitle": "We seems to have a problem!",
+                        "errorDescription": ValidationException.USER_ALREADY_EXISTS,
+                    }
+                ]
+            }
+            return (
+                {
+                    "responseMessage": "Validation error",
+                    "responseCode": 400,
+                    "response": response,
+                },
+                400,
+            )
 
         user_register_validate = UserRegisterValidate(data)
         validate_error = user_register_validate.validate_login()
