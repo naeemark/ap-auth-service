b9b2eb570b05b05fa679f05dbb70e0eb990c41f2
wajahatahmed <wajahatahmad056@gmail.com>
2020-06-05 16:36:27 +0500

    multiple errors handle

--- src/utils/errors/__init__.py
+++ src/utils/errors/__init__.py
@@ -20,15 +20,10 @@ class ErrorManager:
     @classmethod
     def response(cls, *argv, jsonify_response=False):
         """argv = (error code, error title ,error description, status_code, response message)"""
-        error_detail = {
-            "errors": [
-                {
-                    "errorCode": argv[0],
-                    "errorTitle": argv[1],
-                    "errorDescription": argv[2],
-                }
-            ]
-        }
+
+        error_detail = cls.is_iterative_error(
+            error_code=argv[0], error_title=argv[1], error_description=argv[2]
+        )
 
         response = {
             "responseMessage": argv[4],
@@ -39,3 +34,32 @@ class ErrorManager:
             return jsonify(response), argv[3]
 
         return response, argv[3]
+
+    @classmethod
+    def is_iterative_error(cls, **kwargs):
+        """multiple errors control"""
+        error = {"errors": []}
+        error_list = error.get("errors")
+        error_code = kwargs.get("error_code")
+        error_title = kwargs.get("error_title")
+        error_description = kwargs.get("error_description")
+
+        if isinstance(error_description, list) or isinstance(error_description, tuple):
+            for error_description_index in error_description:
+                error_dict = {
+                    "errorCode": error_code,
+                    "errorTitle": error_title,
+                    "errorDescription": error_description_index,
+                }
+                error_list.append(error_dict)
+            return error
+
+        else:
+            error_list.append(
+                {
+                    "errorCode": error_code,
+                    "errorTitle": error_title,
+                    "errorDescription": error_description,
+                }
+            )
+            return error
