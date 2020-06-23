c274d21686b5e041c50b80cfa47bc70eedc92dc2
wajahatahmed <wajahatahmad056@gmail.com>
2020-06-23 16:45:28 +0500

    removed conftest.py from user model

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -2,13 +2,28 @@
     A file to containe all unit tests of
     UserModel
 """
+import datetime
 import os
 
 import fakeredis
 from redis.exceptions import ConnectionError as RedisConnection
+from src import create_app
 from src.models.user import UserModel
 from src.utils.blacklist_manager import BlacklistManager
 from src.utils.constant.response_messages import REDIS_CONNECTION
+
+
+def context():
+    """unit test instance for app and redis"""
+    redis_instance = fakeredis.FakeStrictRedis()
+    flask_app = create_app("flask_test.cfg")
+    flask_app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(
+        minutes=int(os.environ["JWT_ACCESS_TOKEN_EXPIRES_MINUTES"])
+    )
+    flask_app.config["JWT_REFRESH_TOKEN_EXPIRES"] = datetime.timedelta(
+        days=int(os.environ["JWT_REFRESH_TOKEN_EXPIRES_DAYS"])
+    )
+    return flask_app, redis_instance
 
 
 def new_user():
@@ -45,9 +60,9 @@ def test_all_methods_present():
     assert "delete_from_db" in list_of_methods
 
 
-def test_blacklist_manager(test_context):
+def test_blacklist_manager():
     """fake redis test"""
-
+    test_context = context()
     blacklist_manager = BlacklistManager()
     seconds = test_context[0].config["JWT_ACCESS_TOKEN_EXPIRES"].seconds
     blacklist_manager.revoke_token("3", "1231231Xdfwefwe", seconds)
@@ -55,8 +70,9 @@ def test_blacklist_manager(test_context):
     assert isinstance(black_list, list)
 
 
-def test_jwt_life_span(test_context):
+def test_jwt_life_span():
     """life span check"""
+    test_context = context()
 
     token_expire_seconds = test_context[0].config["JWT_ACCESS_TOKEN_EXPIRES"].seconds
     refresh_token_expire_days = test_context[0].config["JWT_REFRESH_TOKEN_EXPIRES"].days
@@ -75,8 +91,9 @@ def test_redis():
     assert redis_instance.get("1").decode() == "abc"
 
 
-def test_redis_failure(test_context):
+def test_redis_failure():
     """connection test"""
+    test_context = context()
 
     test_context[0].config["ENV"] = "redis_test"
     try:
@@ -86,8 +103,9 @@ def test_redis_failure(test_context):
         assert str(error) == REDIS_CONNECTION
 
 
-def test_fake_redis(test_context):
+def test_fake_redis():
     """fake redis test"""
+    test_context = context()
 
     BlacklistManager.initialize_redis(test_context[0].config)
     token_revoke_status = BlacklistManager().revoke_token(
