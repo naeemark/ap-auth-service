309a3045c25795de0af3150cf0de741f7245139c
wajahatahmed <wajahatahmad056@gmail.com>
2020-06-24 19:40:58 +0500

    unnecessary code removed

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -28,12 +28,3 @@ def test_json_user_model():
     """test json function """
 
     assert isinstance(new_user().json(), dict)
-
-
-def test_all_methods_present():
-    """test methods """
-    list_of_methods = new_user().__dir__()
-    assert "save_to_db" in list_of_methods
-    assert "find_by_email" in list_of_methods
-    assert "find_by_id" in list_of_methods
-    assert "delete_from_db" in list_of_methods
