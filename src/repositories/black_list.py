991045ca93b7c5e499eacde5031b019bc6050903
Naeem <naeemark@gmail.com>
2020-07-08 21:17:03 +0800

    completes logout

---
+++ src/repositories/black_list.py
@@ -0,0 +1,60 @@
+"""
+  Test
+"""
+import datetime
+import os
+
+from dynamorm import DynaModel
+from marshmallow import fields
+
+SLUG_ENTITY_HASH_KEY = "#AP-AUTH-BLACKLISTED#"
+
+
+class Blacklist(DynaModel):
+    """  Test """
+
+    class Table:
+        """  Test """
+
+        name = os.environ.get("DYNAMODB_TABLE_NAME_BLACKLIST")
+        hash_key = "entity_hash_key"
+        range_key = "token_id"
+
+    class Schema:
+        """  Test """
+
+        entity_hash_key = fields.String(default=SLUG_ENTITY_HASH_KEY)
+        token_id = fields.String(required=True)
+        type = fields.String(default="access")
+        time_to_live = fields.Integer(required=True)
+        created_at = fields.DateTime(default=datetime.datetime.utcnow())
+
+    def save(self):
+        """  Overridden Save """
+        super(Blacklist, self).save()
+        self.log()
+
+    @classmethod
+    def get(cls, token_id=None):
+        """  Overridden Get """
+        return super(Blacklist, cls).get(entity_hash_key=SLUG_ENTITY_HASH_KEY, token_id=token_id)
+
+    def json(self):
+        """  Test """
+        return {"token_id": self.token_id, "type": self.type, "time_to_live": self.time_to_live}
+
+    def __repr__(self):
+        """  Test """
+        return "<Blacklisted(token_id={}, token_id={}, time_to_live={})>".format(
+            self.token_id, self.type, self.time_to_live
+        )
+
+    def log(self):
+        """  Logs Representation """
+        print(self)
+
+
+# if os.environ.get("DYNAMODB_LOCAL_ENDPOINT"):
+#     print("Connected: Local DYNAMODB")
+#     # pylint: disable=no-member
+#     Blacklist.Table.get_resource(endpoint_url=os.environ.get("DYNAMODB_LOCAL_ENDPOINT"))
--- src/resources/user/logout.py
+++ src/resources/user/logout.py
@@ -1,15 +1,15 @@
 """
   User Logout Resource
 """
+from datetime import datetime
+
 from flask_jwt_extended import get_raw_jwt
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from redis.exceptions import ConnectionError as RedisConnectionUser
+from src.repositories.black_list import Blacklist
 from src.utils.constant.response_messages import LOGOUT
-from src.utils.constant.response_messages import REDIS_CONNECTION
 from src.utils.response_builder import get_error_response
 from src.utils.response_builder import get_success_response
-from src.utils.token_manager import blacklist_tokens
 
 
 class LogoutUser(Resource):
@@ -24,9 +24,18 @@ class LogoutUser(Resource):
         """
 
         try:
-            # blacklist JWT Tokens for the user
-            blacklist_tokens(get_raw_jwt())
+            raw_jwt = get_raw_jwt()
+
+            id_access_token = raw_jwt["jti"]
+            ttl_access_token = datetime.fromtimestamp(raw_jwt["exp"]).timestamp()
+
+            id_refresh_token = raw_jwt["identity"]["refreshTokenId"]
+            ttl_refresh_token = datetime.fromtimestamp(raw_jwt["identity"]["refreshTokenExpire"]).timestamp()
+
+            Blacklist(token_id=id_access_token, type="access", time_to_live=ttl_access_token).save()
+            Blacklist(token_id=id_refresh_token, type="refresh", time_to_live=ttl_refresh_token).save()
+
             return get_success_response(message=LOGOUT)
-        except (RedisConnectionUser, AttributeError) as error:
+        except (AttributeError) as error:
             print(error)
-            return get_error_response(status_code=503, message=REDIS_CONNECTION)
+            return get_error_response(status_code=503, message=str(error))
